fecdb39b1a02db7191eac809422f753684e8c2e3
kamaqiyasov <kamil181994@gmail.com>
2025-12-25 16:02:01 +0300

    Add phone model

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,16 +2,36 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from django.utils.text import slugify
 
 class Command(BaseCommand):
+    help = 'Импорт данных телефонов из CSV файла'
+    
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            '--filename',
+            type=str,
+            default='phones.csv',
+            help='Имя файла для импорта'
+        )
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        filename = options['filename']
+        with open(filename, 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            name = phone.get('name', '')
+            slug = slugify(phone.get('name', '').strip())
+            _, created = Phone.objects.get_or_create(
+                slug = slug,
+                defaults={
+                    'name': name,
+                    'price': float(phone.get('price', 0)),
+                    'image': phone.get('image', ''),
+                    'release_date': phone.get('release_date'),
+                    'lte_exists': phone['lte_exists'] == 'True',
+                }
+            )
+            if created:
+                print(f"Добавлен: {name} (slug: {slug})")
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort_by = request.GET.get('sort', False)
     template = 'catalog.html'
-    context = {}
+    phones_obj = Phone.objects.all()
+    SORT_MAPPING = {
+        'min_price': 'price',
+        'max_price': '-price',
+        'name': 'name'
+    }
+    if sort_by:
+        sort_field = SORT_MAPPING.get(sort_by, 'name')
+        phones_obj = phones_obj.order_by(sort_field)
+    
+    context = {
+        'phones': phones_obj
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_obj = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone_obj
+    }
     return render(request, template, context)
